ddce44452bf47cdc09beadeb76940e9346d15a7e
Daryl Kevin <daryl.kevin.24@gmail.com>
2020-10-10 16:01:37 +0800

    Bug: Fix coordinates for clinic

--- static/js/leaflet-map.js
+++ static/js/leaflet-map.js
@@ -1,11 +1,11 @@
 function initMap() {
-    var latLong = [14.328770, 121.08985]; // Wiesbaden
+    var latLong = [14.331730, 121.049790]; // Laguna, PH
 
     var map = L.map("map").setView(latLong, 5);
 
     L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-      maxZoom: 18,
+      maxZoom: 25,
     }).addTo(map);
 
     var icon = L.icon({
